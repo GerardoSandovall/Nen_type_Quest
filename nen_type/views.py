293376cc777e48c9969f463ed9148f3c90f33caf
gerardo <00014524@uca.edu.sv>
2024-11-02 16:28:50 -0600

    feat: Preguntas, respuestas oficiales y llamar las respuestas en el backend

--- nen_type/views.py
+++ nen_type/views.py
@@ -9,18 +9,42 @@ def Quest(request, Key):
     except ValueError:
         return render(request, 'Quest.html', {'Key': Num, 'error': 'Esta pagina no existe'})
 
-    if 1 <= Key <= 10:
+    if 1 <= Key <= 6:
         if request.method == 'GET':
             return render(request, 'Quest.html', {'Key': Num, 'error': ''})
         else:
             answer = request.POST.get('question')
             print(request.path)  #path de donde viene el post
-            print("/Quest/"+str(Key)) #Comprobar que la respuesta viene de ahi 
+            Path = "/Quest/"+str(Key) #Comprobar que la respuesta viene de ahi
+            
+            if request.path == Path:
+                if answer == '1':
+                    print("Potenciador")
+                    print(answer)
+                elif answer == '2':
+                    print("Emisor")
+                    print(answer)
+                elif answer == '3':
+                    print("Transmutador")
+                    print(answer)
+                elif answer == '4':
+                    print("Manipulador")
+                    print(answer)
+                elif answer == '5':
+                    print("Conjurador")
+                    print(answer)
+                elif answer == '6':
+                    print("Especialista")
+                    print(answer)
+                else:
+                    print("Error")
+                    print(Key)
+                    print(answer)
             
             return redirect('Quest', Key)
     else:
         if request.method == 'GET':
             return render(request, 'Quest.html', {'Key': Num, 'error': 'Esta pagina no existe'})
         else:
-            return redirect('Main')  # Ensure to return redirect
+            return redirect('Main')
 
