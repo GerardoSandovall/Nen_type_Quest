a14eccc40aa163580adba5b51d71a01556072969
gerardo <00014524@uca.edu.sv>
2024-11-03 18:55:40 -0600

    feat: Storing scores in the db

--- nen_type/models.py
+++ nen_type/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class NenPoints(models.Model):
-    Enhancer = models.IntegerField()
-    Transmutator = models.IntegerField()
-    Emitter = models.IntegerField()
-    Conjurer = models.IntegerField()
-    Manipulator = models.IntegerField()
-    Specialist = models.IntegerField()
+    Enhancer = models.IntegerField(default=0)
+    Transmutator = models.IntegerField(default=0)
+    Emitter = models.IntegerField(default=0)
+    Conjurer = models.IntegerField(default=0)
+    Manipulator = models.IntegerField(default=0)
+    Specialist = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    answered_fiel = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.user.username} - Nen Points"
--- nen_type/views.py
+++ nen_type/views.py
@@ -1,50 +1,108 @@
-from django.shortcuts import render, redirect
+
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import NenPoints
 
 @login_required
 def Quest(request, Key):
-    Num = Key
+    # Convert Key to integer if possible, or show an error if not
     try:
         Key = int(Key)
     except ValueError:
-        return render(request, 'Quest.html', {'Key': Num, 'error': 'Esta pagina no existe'})
+        return render(request, 'Quest.html', {'Key': Key, 'error': 'This page does not exist'})
 
+    # Valid range check for Key
     if 1 <= Key <= 6:
+        # GET Request - Show question page
         if request.method == 'GET':
-            return render(request, 'Quest.html', {'Key': Num, 'error': ''})
-        else:
+            Type_is_know, created = NenPoints.objects.get_or_create(user=request.user)
+            if Type_is_know.answered_fiel == False:
+                return render(request, 'Quest.html', {'Key': Key, 'error': ''})
+            else:
+                return render(request, 'Quest.html', {'Key': Key, 'error': 'Updating quest'})
+        
+        # POST Request - Process answer submission
+        elif request.method == 'POST':
             answer = request.POST.get('question')
-            print(request.path)  #path de donde viene el post
-            Path = "/Quest/"+str(Key) #Comprobar que la respuesta viene de ahi
+            if not answer:
+                return render(request, 'Quest.html', {'Key': Key, 'error': 'Please select an option'})
+
+            # Retrieve or create NenPoints object for the user
+            nen_points, created = NenPoints.objects.get_or_create(user=request.user)
+
+            # Assign points based on the answer selected
+            if answer == '1':
+                nen_points.Enhancer += 10
+            elif answer == '2':
+                nen_points.Emitter += 10
+            elif answer == '3':
+                nen_points.Transmutator += 10
+            elif answer == '4':
+                nen_points.Manipulator += 10
+            elif answer == '5':
+                nen_points.Conjurer += 10
+            elif answer == '6':
+                nen_points.Specialist += 10
+            else:
+                return render(request, 'Quest.html', {'Key': Key, 'error': 'Invalid answer'})
             
-            if request.path == Path:
+            Points = get_object_or_404(NenPoints, user= request.user)
+            Validation = Points.Enhancer + Points.Emitter + Points.Transmutator + Points.Manipulator + Points.Conjurer + Points.Specialist
+
+            if Validation == 50:
                 if answer == '1':
-                    print("Potenciador")
-                    print(answer)
+                    nen_points.Enhancer = 10
+                    nen_points.Emitter = 0
+                    nen_points.Transmutator = 0
+                    nen_points.Manipulator = 0
+                    nen_points.Conjurer = 0
+                    nen_points.Specialist = 0
                 elif answer == '2':
-                    print("Emisor")
-                    print(answer)
+                    nen_points.Enhancer = 0
+                    nen_points.Emitter = 10
+                    nen_points.Transmutator = 0
+                    nen_points.Manipulator = 0
+                    nen_points.Conjurer = 0
+                    nen_points.Specialist = 0
                 elif answer == '3':
-                    print("Transmutador")
-                    print(answer)
+                    nen_points.Enhancer = 0
+                    nen_points.Emitter = 0
+                    nen_points.Transmutator = 10
+                    nen_points.Manipulator = 0
+                    nen_points.Conjurer = 0
+                    nen_points.Specialist = 0
                 elif answer == '4':
-                    print("Manipulador")
-                    print(answer)
+                    nen_points.Enhancer = 0
+                    nen_points.Emitter = 0
+                    nen_points.Transmutator = 0
+                    nen_points.Manipulator = 10
+                    nen_points.Conjurer = 0
+                    nen_points.Specialist = 0
                 elif answer == '5':
-                    print("Conjurador")
-                    print(answer)
+                    nen_points.Enhancer = 0
+                    nen_points.Emitter = 0
+                    nen_points.Transmutator = 0
+                    nen_points.Manipulator = 0
+                    nen_points.Conjurer = 10
+                    nen_points.Specialist = 0
                 elif answer == '6':
-                    print("Especialista")
-                    print(answer)
-                else:
-                    print("Error")
-                    print(Key)
-                    print(answer)
-            
+                    nen_points.Enhancer = 0
+                    nen_points.Emitter = 0
+                    nen_points.Transmutator = 0
+                    nen_points.Manipulator = 0
+                    nen_points.Conjurer = 0
+                    nen_points.Specialist = 10
+                nen_points.save()
+            else:
+                nen_points.save()
+                if Validation == 40:
+                    nen_points.answered_fiel = True
+                    nen_points.save()
+
+
+            # Redirect to the next question or a final page if Key == 6
             return redirect('Quest', Key)
-    else:
-        if request.method == 'GET':
-            return render(request, 'Quest.html', {'Key': Num, 'error': 'Esta pagina no existe'})
-        else:
-            return redirect('Main')
+
+    # Handle invalid Key range for GET and POST requests
+    return render(request, 'Quest.html', {'Key': Key, 'error': 'This page does not exist'})
 
